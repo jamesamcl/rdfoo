9908965008fa061b952cd14f1f98553ae8b253b2
James Alastair McLaughlin <james@mclgh.net>
2019-10-07 17:58:42 +0100

    export Watcher

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 
 import Graph from './rdfoo/Graph'
+import { Watcher } from './rdfoo/Graph'
 import Facade from './rdfoo/Facade'
 import * as node from './rdfoo/node'
 import * as triple from './rdfoo/triple'
 import serialize from './rdfoo/serialize'
+import changeURIPrefix from './rdfoo/changeURIPrefix'
 
-export { Graph, Facade, node, triple, serialize }
+export { Graph, Watcher, Facade, node, triple, serialize, changeURIPrefix }
 
